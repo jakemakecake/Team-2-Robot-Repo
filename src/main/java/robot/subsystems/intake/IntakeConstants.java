e8b7e091c078e16cc2cea3ade07717d65f0ce74e
vostsoldier <fantasyindeath86@gmail.com>
2025-02-13 16:25:13 -0500

    intake constants

--- src/main/java/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/robot/subsystems/intake/IntakeConstants.java
@@ -1,5 +1,16 @@
 package robot.subsystems.intake;
 
 public class IntakeConstants {
-    
+    public static final int MAX_VOLT = 0;
+    public static final int MIN_VOLT = 0;
+    public static final int INTAKE_START_ANGLE = 0;
+    public static final int INTAKE_FINAL_ANGLE = 0;
+    public static final int INTAKE_IDLE_ANGLE = 0;
+    public static final int INTAKE_IDLE_VOLT = 0;
+    public static final double MAX_INTAKE_SPEED = 0;
+    public static final double MIN_INTAKE_SPEED = 0;
+    public static final int ROLLER_MAX_SPEED = 0;
+    public static final int ROLLER_MIN_SPEED = 0;
+    public static final int ROLLER_MAX_VOLT = 0;
+    public static final int ROLLER_MIN_VOLT = 0;
 }
