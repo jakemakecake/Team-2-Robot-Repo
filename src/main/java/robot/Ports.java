2afb38f91fb1d1dab0309cdc51ffea07fca6a0df
StarlitHaruka <harukasmain@gmail.com>
2025-02-13 21:44:33 +0000

    added diffdrive steps

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -6,4 +6,12 @@ public final class Ports {
     public static final int OPERATOR = 0;
     public static final int DRIVER = 1;
   }
+
+  public static final class Drive {
+
+    public static int RIGHT_LEADER = 2;
+    public static int RIGHT_FOLLOWER = 3;
+    
+  }
 }
+
