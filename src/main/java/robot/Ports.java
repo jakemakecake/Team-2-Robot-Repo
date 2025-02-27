72fb2c1d063adcc2d2dbac915f4901a2cd53f70d
StarlitHaruka <harukasmain@gmail.com>
2025-02-27 21:01:33 +0000

    progress

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -8,10 +8,10 @@ public final class Ports {
   }
 
   public static final class Drive {
-
     public static int RIGHT_LEADER = 2;
     public static int RIGHT_FOLLOWER = 3;
-    
+    public static int LEFT_LEADER = 4;
+    public static int LEFT_FOLLOWER = 5;
   }
 }
 
---
+++ src/main/java/robot/drive/Drive.java
@@ -0,0 +1,41 @@
+package robot.drive;
+
+//imports
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+
+import java.util.List;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class Drive extends SubsystemBase {
+    private final CANSparkMax leftLeader = new CANSparkMax(Ports.Drive.LEFT_LEADER, MotorType.kBrushless);
+    private final CANSparkMax leftFollower = new CANSparkMax(Ports.Drive.LEFT_FOLLOWER, MotorType.kBrushless);
+    private final CANSparkMax rightLeader = new CANSparkMax(Ports.Drive.RIGHT_LEADER, MotorType.kBrushless);
+    private final CANSparkMax rightFollower = new CANSparkMax(Ports.Drive.RIGHT_FOLLOWER, MotorType.kBrushless);
+
+    public Drive() {
+    for (CANSparkMax spark : List.of(leftLeader, leftFollower, rightLeader, rightFollower)) {
+	    spark.restoreFactoryDefaults();
+      spark.setIdleMode(IdleMode.kBrake);
+    }
+
+    rightFollower.follow(rightLeader);
+    leftFollower.follow(leftLeader);
+
+    leftLeader.setInverted(true);
+  }
+
+  private void drive(double leftSpeed, double rightSpeed) {
+    leftLeader.set(leftSpeed);
+    rightLeader.set(rightSpeed);
+
+  }
+
+
+}
+
+
+
+
