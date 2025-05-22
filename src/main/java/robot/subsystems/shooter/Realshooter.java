5305c58ac5d4b5dcd2b94ee4b0237bacb8595efa
Ethan-Tan-2028 <ethant153@nycstudents.net>
2025-05-22 16:44:26 -0400

    Not yet fix shooter

--- src/main/java/robot/subsystems/shooter/Realshooter.java
+++ src/main/java/robot/subsystems/shooter/Realshooter.java
@@ -1,1 +1,61 @@
+package robot.subsystems.shooter;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.units.Voltage;
+
+public class Realshooter implements ShooterIO{
+    
+ 
+//declaring hardware stuff
+    private final CANSparkMax leftShooterMotor;
+    private final CANSparkMax rightShooterMotor;
+    private final RelativeEncoder leftEncoder;
+    private final RelativeEncoder rightEncoder;
+
+//actual function
+    public Realshooter(){
+        leftShooterMotor = new CANSparkMax(10, MotorType.kBrushless);
+        rightShooterMotor = new CANSparkMax(11,MotorType.kBrushless);
+        leftEncoder =  leftShooterMotor.getEncoder();
+        rightEncoder =  rightShooterMotor.getEncoder();
+        leftShooterMotor.setInverted(true);
+
+
+ 
+
+
+
+    }
+    
+    @Override
+    public void setVoltage(double Voltage) {
+        leftShooterMotor.setVoltage(Voltage);
+    }
+
+    @Override
+    public double getVelocity() {
+    }
+
+	
+
+
+
+
+
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
 
