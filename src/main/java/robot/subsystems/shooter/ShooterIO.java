60beb4cea9c96ec2468c7d23f253401dfaabbc25
Ethan-Tan-2028 <ethant153@nycstudents.net>
2025-05-22 16:43:32 -0400

    Fix Shooter

--- src/main/java/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/robot/subsystems/shooter/ShooterIO.java
@@ -1,51 +1,8 @@
 package robot.subsystems.shooter;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
 
-/**
- * ShooterIO controls the shooter and intake motors.
- */
-public class ShooterIO extends SubsystemBase {
+public interface ShooterIO {
+    public void setVoltage();
 
-    private final CANSparkMax shooterMotor;
-    private final CANSparkMax intakeMotor;
-
-    private double shooterSpeed = 0.0;
-    private double intakeSpeed = 0.0;
-
-    public ShooterIO(int shooterID, int intakeID) {
-        shooterMotor = new CANSparkMax(shooterID, MotorType.kBrushless);
-        intakeMotor = new CANSparkMax(intakeID, MotorType.kBrushless);
-
-        shooterMotor.restoreFactoryDefaults();
-        intakeMotor.restoreFactoryDefaults();
-    }
-
-    public void setShooterSpeed(double speed) {
-        shooterSpeed = speed;
-        shooterMotor.set(speed);
-    }
-
-    public void setIntakeSpeed(double speed) {
-        intakeSpeed = speed;
-        intakeMotor.set(speed);
-    }
-
-    public void runShooterAndIntake(double shooterSpeed, double intakeSpeed) {
-        setShooterSpeed(shooterSpeed);
-        setIntakeSpeed(intakeSpeed);
-    }
-
-    public void stopAll() {
-        setShooterSpeed(0.0);
-        setIntakeSpeed(0.0);
-    }
-
-    @Override
-    public void periodic() {
-        System.out.println("Shooter Speed: " + shooterSpeed + ", Intake Speed: " + intakeSpeed);
-    }
+    public double getVelocity();
 }
-
