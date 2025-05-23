8111ffb6ec3115d0257b9299fd796fc13b978caa
StarlitHaruka <harukasmain@gmail.com>
2025-05-23 09:26:52 -0400

    fixing

--- src/main/java/robot/subsystems/shooter/shooterConstants.java
+++ src/main/java/robot/subsystems/shooter/shooterConstants.java
@@ -1,1 +1,7 @@
+package robot.subsystems.shooter;
 
+public class ShooterConstants {
+    private final int LEFT_SHOOTER_MOTOR_ID = 1; //  change later to  CAN ID
+    private final int RIGHT_SHOOTER_MOTOR_ID = 2; // change later to  CAN ID
+    private final double SHOOTER_DEFAULT_SPEED = 0.7; // 70% (0-1) power, change as needed
+}
