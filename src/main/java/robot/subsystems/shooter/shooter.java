14d7556bddaef8ee5c7cc6e7199cb82a1371381d
Ethan-Tan-2028 <ethant153@nycstudents.net>
2025-05-29 16:56:35 -0400

    Shooterjava not ficxed yet

--- src/main/java/robot/subsystems/shooter/shooter.java
+++ src/main/java/robot/subsystems/shooter/shooter.java
@@ -1,20 +1,44 @@
 package robot.subsystems.shooter;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import edu.wpi.first.math.MathUtil;
 // imports
-
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.constraint.MaxVelocityConstraint;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.subsystems.shooter.shooterConstants.FF;
+import robot.subsystems.shooter.shooterConstants.PID;
+import robot.subsystems.shooter.shooterConstants;
 
-public class Shooter extends SubsystemBase {
+public class shooter extends SubsystemBase {
+  private final PIDController leftpid = new PIDController(PID.kP, PID.kI, PID.kD);
+  private final PIDController rightpid = new PIDController(PID.kP, PID.kI, PID.kD);
+  private final SimpleMotorFeedforward leftFF = new SimpleMotorFeedforward(FF.kS, FF.kA, FF.kV);
+  private final SimpleMotorFeedforward rightFF = new SimpleMotorFeedforward(FF.kS, FF.kA, FF.kV);
+
 
   private ShooterIO rightShooterMotor;
   private ShooterIO leftShooterMotor;
   
-    public Shooter(ShooterIO leftShooterMotor, ShooterIO rightShooterMotor) {
+    public void Shooter(ShooterIO leftShooterMotor, ShooterIO rightShooterMotor) {
       this.leftShooterMotor = leftShooterMotor;
       this.rightShooterMotor = rightShooterMotor;
+      setDefaultCommand(run(()-> update(0)));
+      
 
   }
  
+  public void update(double VelocitySetPoint)
+  {
+    double velocity =
+      Double.isNaN(VelocitySetPoint)
+      ? Defaultvelocity,
+      : MathUtil.clamp(VelocitySetPoint,-MaxVelocity, MaxVelocity)
 
+
+
+  }
 
 
 }
