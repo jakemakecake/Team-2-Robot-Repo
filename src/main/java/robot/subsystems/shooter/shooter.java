a5b6e2ebbf062f2e3d1c329f4d40f4d1b2ae4b93
StarlitHaruka <harukasmain@gmail.com>
2025-06-12 15:58:42 -0400

    working

--- src/main/java/robot/subsystems/shooter/shooter.java
+++ src/main/java/robot/subsystems/shooter/shooter.java
@@ -1,16 +1,22 @@
 package robot.subsystems.shooter;
 // imports
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class Shooter extends SubsystemBase {
 
-  private ShooterIO rightShooterMotor;
-  private ShooterIO leftShooterMotor;
+  private final ShooterIO rightShooterMotor;
+  private final ShooterIO leftShooterMotor;
+
+  private final PIDController leftPidController = new PIDController(0, 0, 0);
   
-    public Shooter(ShooterIO leftShooterMotor, ShooterIO rightShooterMotor) {
+  public Shooter(ShooterIO leftShooterMotor, ShooterIO rightShooterMotor) {
       this.leftShooterMotor = leftShooterMotor;
       this.rightShooterMotor = rightShooterMotor;
+
+
 
   }
  
